6c50feca064487b467c58a93dc4c0441c1d46caa
chongwyb <chongwyb@gmail.com>
2014-09-13 14:04:05 +0800

    Added done method (aka delete) clears individual task in a date clears all task in a date

--- V0.0/src/org/eclipse/wb/swt/Draft1.java
+++ V0.0/src/org/eclipse/wb/swt/Draft1.java
@@ -45,7 +45,7 @@ public class Draft1 {
 	/**
 	 * Launch the application.
 	 */
-	
+
 	// Trial command to push
 	// trial merge update
 	// Trial For Made
@@ -197,305 +197,378 @@ public class Draft1 {
 		frame.getContentPane().add(date8);
 		date8.setBorder(BorderFactory.createCompoundBorder(border,
 				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
-		
-				commandBox = new JTextField();
-				commandBox.addActionListener(new ActionListener() {
-					private BufferedWriter bw;
 
-					public void actionPerformed(ActionEvent arg0) {
-						String input = commandBox.getText();
-						String arrString[] = input.split(" ", 2);
-						String command = arrString[0];
-						String theRest = arrString[1];
+		commandBox = new JTextField();
+		commandBox.addActionListener(new ActionListener() {
+			private BufferedWriter bw;
 
-						switch (command) {
-						case "start":
-							displayAll();
-							// empty the command box
-							commandBox.setText("");
-							break;
+			public void actionPerformed(ActionEvent arg0) {
+				String input = commandBox.getText();
+				String arrString[] = input.split(" ", 2);
+				String command = arrString[0];
+				String theRest = arrString[1];
 
-						case "edit":
-							String arrString2[] = theRest.split(" ", 3);
-							String date = arrString2[0];
-							String number = arrString2[1];
-							String modification = arrString2[2];
-							try {
-								editTask(date, number, modification);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							break;
+				switch (command) {
+				case "start":
+					displayAll();
+					// empty the command box
+					commandBox.setText("");
+					break;
 
-						}
+				case "edit":
+					String arrString2[] = theRest.split(" ", 3);
+					String date = arrString2[0];
+					String number = arrString2[1];
+					String modification = arrString2[2];
+					try {
+						editTask(date, number, modification);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
+					break;
 
-					// THIS IS STUB
-					private void displayAll() {
-						// TODO Auto-generated method stub
-
-						BufferedReader reader = null;
-
-						// obtain the current date
-						DateFormat dateFormat = new SimpleDateFormat("ddMMyy");
-						Date currDate = new Date();
-						String currDateString = dateFormat.format(currDate);
-						// read the general task file
-						fileName = "general.txt";
+				case "done":
+					System.out.println(theRest);
+					if (theRest.length() == 6) {
 						try {
-							reader = new BufferedReader(new FileReader(fileName));
-						} catch (FileNotFoundException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						try {
-							generalTaskBox.read(reader, this);
+							doneTaskClear(theRest);
 						} catch (IOException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-						
-						//loop to display all the date tasks
-						//NOTE: i starts from 1
-						for (int i = 1; i <10; i++) {
-							// display 9 task starting from today's date
-							fileName = currDateString + ".txt";
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							displayDateTask(i, currDateString);
-							// date1.read(reader, this);
-							String dt = currDateString; // Start date
-							SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy");
-							Calendar c = Calendar.getInstance();
-							try {
-								c.setTime(sdf.parse(dt));
-							} catch (ParseException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							c.add(Calendar.DATE, 1); // number of days to add
-							dt = sdf.format(c.getTime()); // dt is now the new date
-							currDateString = dt; // update the current date for the next
-													// loop
-						}
-					}
-
-					private void displayDateTask(int i, String myDate) {
-						BufferedReader reader = null;
-						// TODO Auto-generated method stub
-						switch (i) {
-						case 1:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date1.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						case 2:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date2.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						case 3:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date3.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						case 4:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date4.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						case 5:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date5.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						case 6:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date6.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						case 7:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date7.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						case 8:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date8.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-						
-						case 9:
-							try {
-								reader = new BufferedReader(new FileReader(fileName));
-							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							try {
-								date9.read(reader, this);
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}break;
-							
-						}
-					}
-
-					private void editTask(String date, String number,
-							String modification) throws IOException {
-
-						fileName = date + ".txt";
-						ArrayList<String> currDateTask;
-						currDateTask = new ArrayList<>();
-
-						// read the content of the file, put in the list
-						BufferedReader br = null;
+					} else {
+						String arrString3[] = theRest.split(" ", 2);
+						String doneDate = arrString3[0];
+						String doneNumber = arrString3[1];
 						try {
-							String curr;
-							br = new BufferedReader(new FileReader(fileName));
-							while ((curr = br.readLine()) != null) {
-								currDateTask.add(curr);
-							}
-						} catch (IOException ee) {
-							ee.printStackTrace();
-						}
-						// delete the specific task in the date
-						int position = Integer.parseInt(number);
-						String toBeRemoved = currDateTask.get(position - 1);
-						currDateTask.remove(toBeRemoved);
-
-						// edit 121212 1 modification
-						// insert the modification into the arrayList
-						currDateTask.add(position - 1, modification);
-
-						// clear the file
-						clearFile(fileName);
-
-						// write to the file
-						writeToFile(currDateTask, fileName);
-
-						// then display in the UI
-						displayAll();
-
-						// set the command box to be empty
-						commandBox.setText("");
-
-					}
-
-					private void writeToFile(ArrayList<String> currDateTask,
-							String fileName) throws IOException {
-						// TODO Auto-generated method stub
-						try {
-							File file = new File(fileName);
-							FileWriter fw = new FileWriter(file.getAbsoluteFile());
-							bw = new BufferedWriter(fw);
-							for (int i = 0; i < currDateTask.size(); i++) {
-
-								bw.write(currDateTask.get(i) + "\n");
-							}
-
-						} catch (IOException ee) {
-							ee.printStackTrace();
-						} finally {
-							// close the writer so that it can write to the file
-							bw.close();
-						}
-						System.out.println("DONE!");
-
-					}
-
-					private void clearFile(String fileName) {
-						// TODO Auto-generated method stub
-						try {
-							File file = new File(fileName);
-							FileWriter fw = new FileWriter(file.getAbsoluteFile());
-							BufferedWriter bw = new BufferedWriter(fw);
-							bw.write("");
-							bw.close();
-						} catch (IOException ee) {
-							ee.printStackTrace();
+							doneTaskSpecified(doneDate, doneNumber);
+						} catch (IOException e) {
+							e.printStackTrace();
 						}
 					}
-				});
-				commandBox.setBounds(94, 21, 302, 25);
-				frame.getContentPane().add(commandBox);
-				commandBox.setColumns(10);
+
+				}
+			}
+			
+			private void doneTaskClear(String date) throws IOException {
+				fileName = date + ".txt";
+				clearFile(fileName);
+				// then display in the UI
+				displayAll();
+
+				// set the command box to be empty
+				commandBox.setText("");
+				
+			}
+
+			private void doneTaskSpecified(String date, String number)
+					throws IOException {
+				fileName = date + ".txt";
+				ArrayList<String> currDateTask;
+				currDateTask = new ArrayList<>();
+
+				// read the content of the file, put in the list
+				BufferedReader br = null;
+				try {
+					String curr;
+					br = new BufferedReader(new FileReader(fileName));
+					while ((curr = br.readLine()) != null) {
+						currDateTask.add(curr);
+					}
+				} catch (IOException ee) {
+					ee.printStackTrace();
+				}
+
+				// delete the specific task in the date
+				int position = Integer.parseInt(number);
+				String toBeRemoved = currDateTask.get(position - 1);
+				currDateTask.remove(toBeRemoved);
+
+				// write to the file
+				writeToFile(currDateTask, fileName);
+
+				// then display in the UI
+				displayAll();
+
+				// set the command box to be empty
+				commandBox.setText("");
+
+			}
+
+			// THIS IS STUB
+			private void displayAll() {
+				// TODO Auto-generated method stub
+
+				BufferedReader reader = null;
+
+				// obtain the current date
+				DateFormat dateFormat = new SimpleDateFormat("ddMMyy");
+				Date currDate = new Date();
+				String currDateString = dateFormat.format(currDate);
+				// read the general task file
+				fileName = "general.txt";
+				try {
+					reader = new BufferedReader(new FileReader(fileName));
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					generalTaskBox.read(reader, this);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				// loop to display all the date tasks
+				// NOTE: i starts from 1
+				for (int i = 1; i < 10; i++) {
+					// display 9 task starting from today's date
+					fileName = currDateString + ".txt";
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					displayDateTask(i, currDateString);
+					// date1.read(reader, this);
+					String dt = currDateString; // Start date
+					SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy");
+					Calendar c = Calendar.getInstance();
+					try {
+						c.setTime(sdf.parse(dt));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					c.add(Calendar.DATE, 1); // number of days to add
+					dt = sdf.format(c.getTime()); // dt is now the new date
+					currDateString = dt; // update the current date for the next
+											// loop
+				}
+			}
+
+			private void displayDateTask(int i, String myDate) {
+				BufferedReader reader = null;
+				// TODO Auto-generated method stub
+				switch (i) {
+				case 1:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date1.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 2:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date2.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 3:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date3.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 4:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date4.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 5:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date5.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 6:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date6.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 7:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date7.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 8:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date8.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				case 9:
+					try {
+						reader = new BufferedReader(new FileReader(fileName));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						date9.read(reader, this);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+
+				}
+			}
+
+			private void editTask(String date, String number,
+					String modification) throws IOException {
+
+				fileName = date + ".txt";
+				ArrayList<String> currDateTask;
+				currDateTask = new ArrayList<>();
+
+				// read the content of the file, put in the list
+				BufferedReader br = null;
+				try {
+					String curr;
+					br = new BufferedReader(new FileReader(fileName));
+					while ((curr = br.readLine()) != null) {
+						currDateTask.add(curr);
+					}
+				} catch (IOException ee) {
+					ee.printStackTrace();
+				}
+				// delete the specific task in the date
+				int position = Integer.parseInt(number);
+				String toBeRemoved = currDateTask.get(position - 1);
+				currDateTask.remove(toBeRemoved);
+
+				// edit 121212 1 modification
+				// insert the modification into the arrayList
+				currDateTask.add(position - 1, modification);
+
+				// clear the file
+				clearFile(fileName);
+
+				// write to the file
+				writeToFile(currDateTask, fileName);
+
+				// then display in the UI
+				displayAll();
+
+				// set the command box to be empty
+				commandBox.setText("");
+
+			}
+
+			private void writeToFile(ArrayList<String> currDateTask,
+					String fileName) throws IOException {
+				// TODO Auto-generated method stub
+				try {
+					File file = new File(fileName);
+					FileWriter fw = new FileWriter(file.getAbsoluteFile());
+					bw = new BufferedWriter(fw);
+					for (int i = 0; i < currDateTask.size(); i++) {
+
+						bw.write(currDateTask.get(i) + "\n");
+					}
+
+				} catch (IOException ee) {
+					ee.printStackTrace();
+				} finally {
+					// close the writer so that it can write to the file
+					bw.close();
+				}
+				System.out.println("DONE!");
+
+			}
+
+			private void clearFile(String fileName) {
+				// TODO Auto-generated method stub
+				try {
+					File file = new File(fileName);
+					FileWriter fw = new FileWriter(file.getAbsoluteFile());
+					BufferedWriter bw = new BufferedWriter(fw);
+					bw.write("");
+					bw.close();
+				} catch (IOException ee) {
+					ee.printStackTrace();
+				}
+			}
+		});
+		commandBox.setBounds(94, 21, 302, 25);
+		frame.getContentPane().add(commandBox);
+		commandBox.setColumns(10);
 	}
 }
