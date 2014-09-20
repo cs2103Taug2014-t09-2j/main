a986e412a178e082c016b1bbbb990ca81737a859
chongwyb <chongwyb@gmail.com>
2014-09-20 21:34:20 +0800

    clear up done class code

--- V0.0/src/org/eclipse/wb/swt/commandDone.java
+++ V0.0/src/org/eclipse/wb/swt/commandDone.java
@@ -1,14 +1,13 @@
 package org.eclipse.wb.swt;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+
+/*
+ * 
+ */
 
 public class commandDone {
 
@@ -21,13 +20,13 @@ public class commandDone {
 	public commandDone(String date, String position) {
 		this.date = date;
 		this.position = Integer.parseInt(position);
-		//System.out.println("executed 1");
+		// System.out.println("executed 1");
 	}
 
+	// All task in a date
 	public commandDone(String date) {
-		// All task in a date
 		this.date = date;
-		//System.out.println("executed 2");
+		// System.out.println("executed 2");
 	}
 
 	// Mutator
@@ -35,39 +34,20 @@ public class commandDone {
 	public void clearDateTaskSpecific() {
 		String fileName = date + ".txt";
 		ArrayList<String> currDateTask = new ArrayList<>();
-		
+
 		// read the content of the file, put in the list
-		BufferedReader br = null;
-		try {
-			String curr;
-			br = new BufferedReader(new FileReader(fileName));
-			while ((curr = br.readLine()) != null) {
-				currDateTask.add(curr);
-			}
-		} catch (IOException ee) {
-			ee.printStackTrace();
-		}
-		
+		currDateTask = (new readFile(fileName)).readContents();
+
 		// check if valid
-		if(position-1 < currDateTask.size()){
-		currDateTask.remove(position-1);
-			System.out.println("Success delete");
-		} else{
-			System.out.println("Failed delete");
+		if (position - 1 < currDateTask.size()) {
+			currDateTask.remove(position - 1);
+			// System.out.println("Success delete");
+		} else {
+			// System.out.println("Failed delete");
 		}
-		
+
 		// write in file
-		try {
-			File file = new File(fileName);
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			for (int i = 0; i < currDateTask.size(); i++) {
-				bw.write(currDateTask.get(i) + "\n");
-			}
-			bw.close();
-		} catch (IOException ee) {
-			ee.printStackTrace();
-		}
+		(new writeFile(fileName, currDateTask)).writeContents();
 
 	}
 
