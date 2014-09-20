8d204979cb987b67e8f6ca8de5d0d4546afc201c
chongwyb <chongwyb@gmail.com>
2014-09-20 21:34:54 +0800

    check in for writeFile class

---
+++ V0.0/src/org/eclipse/wb/swt/writeFile.java
@@ -0,0 +1,34 @@
+package org.eclipse.wb.swt;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class writeFile {
+	String fileName;
+	ArrayList<String> currDateTask;
+
+	// Constructor
+	public writeFile(String fileName, ArrayList<String> currDateTask) {
+		this.fileName = fileName;
+		this.currDateTask = currDateTask;
+	}
+
+	// Mutator
+	public void writeContents() {
+		try {
+			File file = new File(fileName);
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (int i = 0; i < currDateTask.size(); i++) {
+				bw.write(currDateTask.get(i) + "\n");
+			}
+			bw.close();
+		} catch (IOException ee) {
+			ee.printStackTrace();
+		}
+
+	}
+}
