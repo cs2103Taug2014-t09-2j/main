f87edae442af6b31b6a989e95122ebaf2eae016f
chongwyb <chongwyb@gmail.com>
2014-09-20 21:34:37 +0800

    check in of readFile class

---
+++ V0.0/src/org/eclipse/wb/swt/readFile.java
@@ -0,0 +1,35 @@
+package org.eclipse.wb.swt;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class readFile {
+
+	String fileName;
+	ArrayList<String> currDateTask;
+
+	// Constructor
+	public readFile(String fileName) {
+		this.fileName = fileName;
+		currDateTask = new ArrayList<>();
+	}
+
+	// Mutator
+	public ArrayList<String> readContents() {
+		BufferedReader br = null;
+		try {
+			String curr;
+			br = new BufferedReader(new FileReader(fileName));
+			while ((curr = br.readLine()) != null) {
+				currDateTask.add(curr);
+			}
+			br.close();
+		} catch (IOException ee) {
+			ee.printStackTrace();
+		}
+		return currDateTask;
+	}
+
+}
