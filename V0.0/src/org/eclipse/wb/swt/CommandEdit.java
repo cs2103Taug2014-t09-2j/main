781627c5257d889f743b24aab144d581cde24e05
ngocanh2809 <NgocAnh@192.168.0.100>
2014-09-21 11:08:36 +0800

    Handled 2 cases where user can choose to edit either time or task syntax: edit date index specification(time/task) modification

--- V0.0/src/org/eclipse/wb/swt/CommandEdit.java
+++ V0.0/src/org/eclipse/wb/swt/CommandEdit.java
@@ -6,15 +6,44 @@ import java.util.ArrayList;
 public class CommandEdit {
 	private String date;
 	private String index;
-	private String time;
+	private String specification;//specify whether to edit time or task
 	private String modification;
 	
 	private static final String CONTENT_TO_DISPLAY = "[%1$s] %2$s";
 	public CommandEdit(String str1, String str2, String str3, String str4) {
 		date = str1;
 		index = str2;
-		time = str3;
+		specification = str3;
 		modification = str4;
+	}
+	
+	public void edit() throws IOException {
+		if (specification.equals("time")){
+			editTime();
+		} else if (specification.equals("task")){
+			editTask();
+		}
+	}
+	
+	public void editTime() throws IOException {
+
+		String fileName = date + ".txt";
+		ArrayList<String> currDateTask = new ArrayList<String>();
+		currDateTask = (new ReadFile(fileName)).readContents();
+		
+		// delete the task with the given index number in the date
+		int position = Integer.parseInt(index);
+		String toBeRemoved = currDateTask.get(position+1);
+		//Split toBeRemoved string to 2parts and get the task
+		String removed = toBeRemoved.split(" ", 2)[1];
+		currDateTask.remove(toBeRemoved);
+
+		String modificationFinal = String.format(CONTENT_TO_DISPLAY, modification, removed);
+
+		// insert the modification into the arrayList
+		currDateTask.add(position + 1, modificationFinal);
+
+		(new WriteFile(fileName, currDateTask)).writeContents();
 	}
 	
 	public void editTask() throws IOException {
@@ -28,7 +57,10 @@ public class CommandEdit {
 		String toBeRemoved = currDateTask.get(position - 1);
 		currDateTask.remove(toBeRemoved);
 
-		String modificationFinal = String.format(CONTENT_TO_DISPLAY, time, modification);
+		//Split toBeRemoved string to 2parts and get the time
+		String removed = toBeRemoved.split(" ", 2)[0];
+		removed = removed.substring(1, removed.length()-1);
+		String modificationFinal = String.format(CONTENT_TO_DISPLAY, removed, modification);
 
 		// insert the modification into the arrayList
 		currDateTask.add(position - 1, modificationFinal);
