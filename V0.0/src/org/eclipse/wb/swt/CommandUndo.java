c638f5f2a35c312bef2e5c8d97fddacbcb3fbfe5
chongwyb <chongwyb@gmail.com>
2014-09-21 23:41:04 +0800

    Update CommandUndo for add, doneAll

--- V0.0/src/org/eclipse/wb/swt/CommandUndo.java
+++ V0.0/src/org/eclipse/wb/swt/CommandUndo.java
@@ -1,5 +1,8 @@
 package org.eclipse.wb.swt;
 
+import java.io.IOException;
+import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.Stack;
 
 public class CommandUndo {
@@ -10,33 +13,55 @@ public class CommandUndo {
 		commandHistory = new Stack<String>();
 	}
 
-	public void copyAddReverseCommand(String date, int position) {
-		commandHistory.push("done " + date + " " + position);
+	public void copyAddCommandToReverse(String date, String time, String task) {
+		int arrLine = (new CommandSearchCurrDate(date, time, task)).searchString();
+		System.out.println("done " + date + " " + arrLine);
+		commandHistory.push("done " + date + " " + arrLine);
+		
 	}
 
-	public void copyDoneReverseCommand(String date, String time, String task) {
-		commandHistory.push("add " + date + " " + time + " " + task);
+	public void copyDoneCommandToReverseSpecific(String date, int position) {
+
+	}
+	
+	public void copyDoneCommandToReverseAll(String date) {
+		ArrayList<String> currDateTask = new ArrayList<>();
+		currDateTask = (new ReadFile(date+".txt")).readContents();
+		int originalSize = currDateTask.size();
+		for(int i = 0; i < originalSize; i++){
+			String[] str = (currDateTask.get(i)).split(" ", 2);
+			//System.out.println(date + " " + str[0].substring(1,str[0].length()-1) + " " + str[1]);
+			commandHistory.push(date + " " + str[0].substring(1,str[0].length()-1) + " " + str[1]);
+		}
+		commandHistory.push(Integer.toString(originalSize));
 	}
 
-	public void copyEditReverseCommand(String date, int position,
-			String oldTime, String oldTask) {
-		commandHistory.push("edit " + date + " " + position + " " + oldTime
-				+ " " + oldTask);
+	public void copyEditCommandToReverse(String date, int position,
+			String update) {
+
 	}
 
 	// Print command to be executed
-	public void printReverseCommand() {
+	public void runReverseCommand() throws IOException {
 		if (!commandHistory.isEmpty()) {
-			String checkInt = commandHistory.pop();
-			if (checkInt.length() == 1) {
-				// For done all
-				int popNo = Integer.parseInt(checkInt);
+			String checkStr = commandHistory.pop();
+			if (checkStr.length() == 1) {
+				// For done all only, adds back in to date.txt 
+				int popNo = Integer.parseInt(checkStr);
 				for (int i = 0; i < popNo; i++) {
-					System.out.println(commandHistory.pop());
+					//String[] arrStr = commandHistory.pop().split(" ", 3);
+					String str = commandHistory.pop();
+					String[] arrStr = str.split(" ", 3);
+					(new CommandAdd(arrStr[0],arrStr[1],arrStr[2])).addTask();
 				}
 			} else {
 				// For other commands
-				System.out.println(commandHistory.pop());
+				String[] arrStr = checkStr.split(" ", 2);
+				//System.out.println(arrStr[0]);
+				if(arrStr[0].equals("done")){
+					String[] arrStrDS = arrStr[1].split(" ", 2);
+					(new CommandDone(arrStrDS[0],arrStrDS[1])).clearDateTaskSpecific();
+				}
 			}
 		} else {
 			// Unable to undo
