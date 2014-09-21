4caf8266241d314f53f6fd99964fdbe7fb8ee78c
Antony <karukapallyantony@msn.com>
2014-09-21 21:12:06 +0800

    added today/tomorrow, sorting (raw, only for Add) Supported 2-3 format [instead of just 1 time record]
    Updated write file so it will autogenerate the header (210914 \n\n).
    ALL Read files should ignore first 2 lines.

--- V0.0/src/org/eclipse/wb/swt/CommandAdd.java
+++ V0.0/src/org/eclipse/wb/swt/CommandAdd.java
@@ -7,26 +7,62 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CommandAdd {
 	String date;
 	String task;
 	String time;
 
+	private String validateDate(String date){
+		if (date.toLowerCase().equals("today")){
+			DateFormat dateFormat = new SimpleDateFormat("ddMMyy");
+			Date currDate = new Date();
+			return dateFormat.format(currDate);
+		}
+		else if (date.toLowerCase().equals("tomorrow")){
+			DateFormat dateFormat = new SimpleDateFormat("ddMMyy");
+			long currTime = new Date().getTime();
+			Date currDate = new Date(currTime+(24*3600*1000));
+			return dateFormat.format(currDate);
+		}
+		return date;
+	}
+	
 	private String validateTime(String time) throws Exception{
+		// TODO Throw meaningful exception messages
 		if (time.equals("-")){
 			return "-";
 		}
-		int t = Integer.valueOf(time);
-		if (t>=0 && t<=23){
+		String splitted_time[] = time.split("-");
+		boolean valid = true;
+		if (splitted_time.length < 1 || splitted_time.length >2){
+			valid = false;
+		}
+		if (time.length() > 0){
+			if (time.charAt(time.length()-1) == '-'){
+				valid = false;
+			}
+		}
+		int old_t = -1;
+		for (int i=0;i<splitted_time.length;i++){
+			int t = Integer.valueOf(splitted_time[i]);
+			if (t<0 || t>23 || old_t>=t){
+				valid = false;
+			}
+			old_t = t;
+		}
+		if (valid){
 			return time;
 		}
 		throw new Exception("Invalid time.");
 	}
 	
 	public CommandAdd(String date, String time, String task){
-		this.date = date;
+		this.date = validateDate(date);
 		try{
 			this.time = validateTime(time);
 		}
@@ -45,7 +81,12 @@ public class CommandAdd {
 				String line;
 				BufferedReader br = new BufferedReader(new FileReader(
 						fileName));
+				int _ignore = 2;
 				while ((line = br.readLine()) != null) {
+					if (_ignore > 0){ //ignore first 2 lines
+						_ignore -= 1;
+						continue;
+					}
 					list.add(line); // File already exists, add its
 									// content to the list
 				}
@@ -73,6 +114,5 @@ public class CommandAdd {
 		list = isFileExist(fileName);
 		list.add(content);
 		(new WriteFile(fileName, list)).writeContents();
-		//writeToFile(list, fileName);
 	}
 }
