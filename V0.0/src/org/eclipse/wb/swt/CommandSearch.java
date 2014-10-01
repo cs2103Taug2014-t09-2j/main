e6b7dca168cc580ab29e5fa685de66030cc35ef2
ngocanh2809 <NgocAnh@118-135.priv23.nus.edu.sg>
2014-10-01 21:59:48 +0800

    CommandSearch added

---
+++ V0.0/src/org/eclipse/wb/swt/CommandSearch.java
@@ -0,0 +1,93 @@
+package org.eclipse.wb.swt;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class CommandSearch {
+	private String keyword;
+	
+	public CommandSearch(String key) {
+		keyword = key;
+	}
+	
+	public String search() {
+		//The array to store the search result
+		ArrayList<String> searchResult = new ArrayList<String>();
+		
+		try {
+			searchResult.addAll(searchDate());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		searchResult.addAll(searchGeneral());
+		searchResult.addAll(searchMissing());
+		
+		return arrayListToString(searchResult);
+	}
+	
+	public ArrayList<String> searchGeneral() {
+		String fileName = "general.txt";
+		ArrayList<String> generalTask = new ArrayList<String>(); //generalTask contains tasks in General box
+		generalTask = (new ReadFile(fileName)).readContents();
+		
+		ArrayList<String> searchResultGeneral = new ArrayList<String>();
+		for (int i=0; i<generalTask.size(); i++) {
+			if (generalTask.get(i).contains(keyword)) {
+				String result = "General " + Integer.toString(i+1) + ". " + generalTask.get(i);
+				searchResultGeneral.add(result);
+			}
+		}
+		return generalTask;
+	}
+	
+	public ArrayList<String> searchMissing() {
+		String fileName = "missing.txt";
+		ArrayList<String> missingTask = new ArrayList<String>(); //missingTask contains tasks in Missing box
+		missingTask = (new ReadFile(fileName)).readContents();
+		
+		ArrayList<String> searchResultMissing = new ArrayList<String>();
+		for (int i=0; i<missingTask.size(); i++) {
+			if (missingTask.get(i).contains(keyword)) {
+				String result = "Missing " + Integer.toString(i+1) + ". " + missingTask.get(i);
+				searchResultMissing.add(result);
+			}
+		}
+		return missingTask;
+	}
+	
+	public ArrayList<String> searchDate() throws FileNotFoundException{
+		ArrayList<String> searchResultDate = new ArrayList<String>();
+		ArrayList<String> dateTask = new ArrayList<String>();
+		String result = new String();
+		
+		String currDateString = DateUpdate.getCurrDate();
+		for (int i=0; i<7; i++) {
+			if (!dateTask.isEmpty()) {
+				dateTask.clear();
+			}
+			String fileName = currDateString + ".txt";
+			//read file content into an ArrayList
+			dateTask = (new ReadFile(fileName)).readContents();
+			
+			//Search the content of each file, add any match to searchResultDate arraylist
+			for (int j=0; j<dateTask.size(); j++){
+				if (dateTask.get(j).contains(keyword)) {
+					result = currDateString + " " + Integer.toString(j+1) + ". " + dateTask.get(j);
+					searchResultDate.add(result);
+				}
+			}
+			 currDateString = DateUpdate.getNextDate(currDateString);
+		 }
+		 return searchResultDate;
+	}
+	
+	public String arrayListToString(ArrayList<String> arr) {
+		String str = new String();
+		for (int i=0; i<arr.size(); i++) {
+			str += arr.get(i);
+		}
+		return str;
+	}
+}
