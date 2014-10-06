fad6e5f9ad106703e5edab7013d4a03beaa01e8b
Made <radityapujamurti@gmail.com>
2014-10-06 11:10:07 +0800

    building the foundation of Parser class

---
+++ V0.0/src/org/eclipse/wb/swt/Parser.java
@@ -0,0 +1,22 @@
+package org.eclipse.wb.swt;
+
+public class Parser {
+	private String input;
+	private String date;
+	private int index;
+	private String time;
+	private String task;
+	
+	public void setInput(String userInput){
+		input = userInput;
+	}
+	
+	public String getInput(){
+		return input;
+	}
+	
+	public void processInput(String input){
+		
+		
+	}
+}
