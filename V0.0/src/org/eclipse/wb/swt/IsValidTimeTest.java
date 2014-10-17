eb7ed52cf18df555c6bec40d48a992c73975b375
Antony <karukapallyantony@msn.com>
2014-10-17 23:04:10 +0800

    more test

--- V0.0/src/org/eclipse/wb/swt/IsValidTimeTest.java
+++ V0.0/src/org/eclipse/wb/swt/IsValidTimeTest.java
@@ -14,8 +14,8 @@ import org.junit.Test;
 public class IsValidTimeTest {
 
 	@Test
-	public void test() {
-		// String curdate = DateModifier.getCurrDate();
+	public void getFormattedTime_test() {
+		
 		try {
 			assertEquals(IsValidTime.getFormattedTime(0), "0000");
 			assertEquals(IsValidTime.getFormattedTime(1), "0100");
@@ -26,6 +26,7 @@ public class IsValidTimeTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 // passes test when exception is thrown
 		boolean fail = false;
 		try {
@@ -53,4 +54,35 @@ public class IsValidTimeTest {
 		}
 	}
 
+	@Test
+	public void validateTime_test() {
+		try {
+			assertEquals(IsValidTime.validateTime("0"), "0000");
+			assertEquals(IsValidTime.validateTime("-"), "-");
+			assertEquals(IsValidTime.validateTime("8-10"), "0800-1000");
+			assertEquals(IsValidTime.validateTime("830-945"), "0830-0945");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		boolean fail = false;
+		try {
+			IsValidTime.validateTime("");
+			fail = true;
+		}
+		catch (Exception e) {}
+		try {
+			IsValidTime.validateTime("000000000001");
+			fail = true;
+		}
+		catch (Exception e) {}
+		try {
+			IsValidTime.validateTime("299");
+			fail = true;
+		}
+		catch (Exception e) {}
+		if (fail) {
+			fail("fails to fail");
+		}
+	}
 }
