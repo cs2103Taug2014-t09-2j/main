08c33d683fd9d1b53da61da376a2aa71017869fa
ngocanh2809 <NgocAnh@192.168.0.101>
2014-10-23 00:43:00 +0800

    Added jUnit test for CommandEdit

---
+++ V0.0/src/org/eclipse/wb/swt/CommandEditTest.java
@@ -0,0 +1,54 @@
+package org.eclipse.wb.swt;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Ngoc Anh
+ */
+public class CommandEditTest {
+	
+	@Test
+	public void testEditTask() {
+		System.out.println("Test editTask:");
+		CommandEdit cmdEdit = new CommandEdit();
+		cmdEdit.setDate("251014");
+		cmdEdit.setIndex("2");
+		cmdEdit.setMod("have lunch with dad");
+		cmdEdit.setSpec("task");
+		String oldTask = "[1000-1200] have lunch with friends";
+		String newTask = cmdEdit.editTask(oldTask);
+		assertEquals("[1000-1200] have lunch with dad", newTask);
+	}
+	@Test
+	public void testEditTime() {
+		System.out.println("Test editTime:");
+		CommandEdit cmdEdit = new CommandEdit();
+		cmdEdit.setDate("251014");
+		cmdEdit.setIndex("2");
+		cmdEdit.setMod("1300-1400");
+		cmdEdit.setSpec("time");
+		String oldTask = "[1000-1200] have lunch with friends";
+		String newTask = cmdEdit.editTime(oldTask);
+		assertEquals("[1300-1400] have lunch with friends", newTask);
+	}
+	@Test
+	public void testEditAll() {
+		System.out.println("Test editAll:");
+		CommandEdit cmdEdit = new CommandEdit();
+		cmdEdit.setDate("251014");
+		cmdEdit.setIndex("2");
+		cmdEdit.setMod("1300-1400 have lunch with dad");
+		cmdEdit.setSpec("all");
+		String oldTask = "[1000-1200] have lunch with friends";
+		String newTask = cmdEdit.editAll(oldTask);
+		System.out.println(oldTask);
+		assertEquals("[1300-1400] have lunch with dad", newTask);
+	}
+	
+}
