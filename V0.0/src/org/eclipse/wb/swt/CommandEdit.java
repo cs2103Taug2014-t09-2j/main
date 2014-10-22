3616d24cdc6a2ba477e9c0784fea4142dec49404
ngocanh2809 <NgocAnh@192.168.0.101>
2014-10-22 23:42:26 +0800

    Refactored edit command to follow SLAP

--- V0.0/src/org/eclipse/wb/swt/CommandEdit.java
+++ V0.0/src/org/eclipse/wb/swt/CommandEdit.java
@@ -7,23 +7,16 @@ import java.util.logging.Logger;
 
 public class CommandEdit {
 	private String date;
-	private String index;
+	private int index;
 	private String specification;//specify whether to edit time or task
 	private String modification;
 	
 	private final static Logger LOGGER = Logger.getLogger(CommandEdit.class .getName());
 	
-	private static final String TEXT_EXTENSION = ".txt";
+	private static final String TXT_EXTENSION = ".txt";
 	private static final String CONTENT_TO_DISPLAY = "[%1$s] %2$s";
 	
 	public CommandEdit() {
-	}
-	
-	public CommandEdit() {
-		date = str1;
-		index = str2;
-		specification = str3;
-		modification = str4;
 	}
 	
 	//Mutators
@@ -31,7 +24,7 @@ public class CommandEdit {
 		date = str;
 	}
 	public void setIndex(String str) {
-		index = str;
+		index = Integer.parseInt(str);
 	}
 	public void setSpec(String str) {
 		specification = str;
@@ -40,89 +33,74 @@ public class CommandEdit {
 		modification = str;
 	}
 	
-	public void edit(String str1, String str2, String str3, String str4) throws IOException {
+	public void edit(String str1, String str2, String str3, String str4) {
+		LOGGER.setLevel(Level.INFO);
+		
 		setDate(str1); setIndex(str2); setSpec(str3); setMod(str4);
 		
-		LOGGER.setLevel(Level.INFO);
-		
 		String fileName = getDateStr(date);
-		if (specification.equals("time")){
-			editTime(fileName);
-		} else if (specification.equals("task")){
-			editTask(fileName);
-		} else if (specification.equals("all")) {
-			editAll(fileName);
+		
+		ArrayList<String> dateTask = new ArrayList<String>();
+		dateTask = getFileContent(fileName);
+		
+		String oldTask = getTask(dateTask);
+		dateTask.remove(oldTask);
+		String newTask = new String();
+		
+		switch (specification) {
+		case "time":
+			newTask = editTime(oldTask);
+			break;
+		case "task":
+			newTask = editTask(oldTask);
+			break;
+		case "all":
+			newTask = editAll(oldTask);
+			break;
+		default:
+			
 		}
+		dateTask.add(index-1, newTask);
+		(new FileAccessor(fileName, dateTask)).writeContents();
 	}
 	
-	private void editTime(String fileName) throws IOException {
-		LOGGER.setLevel(Level.INFO);
+	private String editTime(String oldTask) {
 		LOGGER.info("Edits the time of a specified task");
 		
-		ArrayList<String> dateTask = new ArrayList<String>();
-		dateTask = (new FileAccessor(fileName)).readContents();
-		
-		// delete the task with the given index number in the date
-		int position = Integer.parseInt(index);
-		String toBeRemoved = dateTask.get(position-1);
-		//Split toBeRemoved string to 2parts and get the task
-		String keep = toBeRemoved.split(" ", 2)[1];
-		dateTask.remove(toBeRemoved);
-
-		String modificationFinal = String.format(CONTENT_TO_DISPLAY, modification, keep);
-
-		// insert the modification into the arrayList
-		dateTask.add(position - 1, modificationFinal);
-
-		(new FileAccessor(fileName, dateTask)).writeContents();
+		//Split toBeRemoved string to 2parts and get the task (the unchanged part)
+		String keep = oldTask.split(" ", 2)[1];
+		return String.format(CONTENT_TO_DISPLAY, modification, keep);
 	}
 	
-	private void editTask(String fileName) throws IOException {
-		LOGGER.setLevel(Level.INFO);
+	private String editTask(String oldTask) {
 		LOGGER.info("Edits the task content of a specified task");
-		
-		ArrayList<String> dateTask = new ArrayList<String>();
-		dateTask = (new FileAccessor(fileName)).readContents();
-		
-		// delete the task with the given index number in the date
-		int position = Integer.parseInt(index);
-		String toBeRemoved = dateTask.get(position - 1);
-		dateTask.remove(toBeRemoved);
 
-		//Split toBeRemoved string to 2parts and get the time
-		String keep = toBeRemoved.split(" ", 2)[0];
+		//Split toBeRemoved string to 2parts and get the time (the unchanged part)
+		String keep = oldTask.split(" ", 2)[0];
 		keep = keep.substring(1, keep.length()-1);
-		String modificationFinal = String.format(CONTENT_TO_DISPLAY, keep, modification);
-
-		// insert the modification into the arrayList
-		dateTask.add(position - 1, modificationFinal);
-
-		(new FileAccessor(fileName, dateTask)).writeContents();
+		return String.format(CONTENT_TO_DISPLAY, keep, modification);
 	}
 	
-	private void editAll(String fileName) throws IOException {
-		LOGGER.setLevel(Level.INFO);
+	private String editAll(String fileName) {
 		LOGGER.info("Edits the whole line of a specified task");
 		
-		ArrayList<String> dateTask = new ArrayList<String>();
-		dateTask = (new FileAccessor(fileName)).readContents();
-		
-		// delete the task with the given index number in the date
-		int position = Integer.parseInt(index);
-		String toBeRemoved = dateTask.get(position - 1);
-		dateTask.remove(toBeRemoved);
-		
 		String[] change = modification.split(" ", 2);
-		String modificationFinal = String.format(CONTENT_TO_DISPLAY, change[0], change[1]);
-		
-		dateTask.add(position - 1, modificationFinal);
-
-		(new FileAccessor(fileName, dateTask)).writeContents();
+		return String.format(CONTENT_TO_DISPLAY, change[0], change[1]);
 	}
 	
 	private String getDateStr(String date){
-		return IsValidDate.validateDate(date) + TEXT_EXTENSION;//get the name of the file given the date input
+		return IsValidDate.validateDate(date) + TXT_EXTENSION;//get the name of the file given the date input
 													//date input can be 1-7
+	}
+	
+	private ArrayList<String> getFileContent(String fileName) {
+		return (new FileAccessor(fileName)).readContents();
+	}
+	
+	//Gets the task string given the index of the task
+	private String getTask(ArrayList<String> dateTask) { 
+		String task = dateTask.get(index -1);
+		return task;
 	}
 }
 
