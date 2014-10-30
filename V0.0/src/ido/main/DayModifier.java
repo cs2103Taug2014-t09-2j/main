7d1e255bd5a61cf407c7ea9e6cd1d4cd9188518f
ngocanh2809 <NgocAnh@172.28.180.175>
2014-10-30 08:45:08 +0800

    Removed unnecessary import

--- V0.0/src/ido/main/DayModifier.java
+++ V0.0/src/ido/main/DayModifier.java
@@ -1,10 +1,7 @@
 package ido.main;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class DayModifier {
 	
