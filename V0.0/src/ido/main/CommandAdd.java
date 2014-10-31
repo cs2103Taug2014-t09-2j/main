a62be074f42c53d968dc6a6b64227f3973320ea5
Antony <karukapallyantony@msn.com>
2014-11-01 00:58:06 +0800

    cleaned up code

--- V0.0/src/ido/main/CommandAdd.java
+++ V0.0/src/ido/main/CommandAdd.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 public class CommandAdd {
 
 	private static Logger logger = Logger.getLogger("CommandAdd");
@@ -21,7 +22,12 @@ public class CommandAdd {
 	String date;
 	String task;
 	String time;
-
+	
+	private static final String ALL_DAY_TASK = "[all-day] ";
+	private static final int DATE_LENGTH = 6;
+	private static final int BOX_VALUE = 1;
+	private static final int TOTAL_BOX_VALUE = 7;
+	
 	public CommandAdd(String date, String time, String task) {
 		this.date = date;
 		try {
@@ -35,10 +41,18 @@ public class CommandAdd {
 
 	public void addTask() {
 		logger.log(Level.INFO, "add processing");
-		int dateLength;
-		dateLength = date.length();
-		if (dateLength != 5)
+		//checking if date is valid
+		int dateLength = date.length();
+		boolean wrongDate = false;
+		if (dateLength!=DATE_LENGTH){
+			wrongDate = true;
+			if(dateLength == BOX_VALUE){
+				wrongDate = false;
+			}	
+		}
+		if (wrongDate)
 			WarningPopUp.infoBox("Invalid Date!", "ERROR");
+		
 		// Create the name of the text file
 		String fileName = date + ".txt";
 		if (date.equals("-")) {
@@ -46,8 +60,9 @@ public class CommandAdd {
 		}
 		String content = "[" + time + "] " + task;
 		if (time.equals("-")) {
-			content = "[all-day] " + task;
+			content = ALL_DAY_TASK + task;
 		}
+		
 		ArrayList<String> list = new ArrayList<String>();
 		// Check if the text file exists, if it does, add its content to
 		// a list and return the list
@@ -57,7 +72,7 @@ public class CommandAdd {
 		(new FileAccessor(fileName, list)).writeContents();
 		
 		boolean dateOutOfBox = true;
-		for (int i = 0; i < 7; i++) {
+		for (int i = 0; i < TOTAL_BOX_VALUE; i++) {
 			if (date.equals(DateModifier.getParticularDate(DateModifier.getCurrDate(), i))) {
 				dateOutOfBox = false;
 				break;
