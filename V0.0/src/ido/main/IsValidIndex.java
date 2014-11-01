fc0087b54a657866fcae08876565984323a4a424
chongwyb <chongwyb@gmail.com>
2014-11-02 00:22:24 +0800

    Added method testEmptyFile

--- V0.0/src/ido/main/IsValidIndex.java
+++ V0.0/src/ido/main/IsValidIndex.java
@@ -5,23 +5,34 @@ import java.util.List;
 public class IsValidIndex {
 	private String filename;
 	private String index;
-	
-	IsValidIndex(String filename, String index){
+
+	IsValidIndex(String filename, String index) {
 		this.filename = filename;
 		this.index = index;
 	}
-	
-	String validateIndex(){
-		List<String> data = (new FileAccessor(filename+".txt")).readContents();
-		int _index = -1; //will fail
-		try{
+
+	public String validateIndex() {
+		List<String> data = (new FileAccessor(filename + ".txt"))
+				.readContents();
+		int _index = -1; // will fail
+		try {
 			_index = Integer.parseInt(index);
-		} catch (Exception e){
-			//do nothing
+		} catch (Exception e) {
+			// do nothing
 		}
-		if (_index>0 && _index<=data.size())
+		if (_index > 0 && _index <= data.size())
 			return index;
 		WarningPopUp.infoBox("Invalid Index!", "ERROR");
-		return ""; //does not do shit
+		return ""; // does not do shit
+	}
+
+	public boolean testEmptyFile() {
+		List<String> data = (new FileAccessor(filename + ".txt"))
+				.readContents();
+		if (data.size() == 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
