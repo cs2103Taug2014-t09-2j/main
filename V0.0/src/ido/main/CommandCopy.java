184aa3cdb96f8796451c9cae5aa0041b5d5f92bd
chongwyb <chongwyb@gmail.com>
2014-11-02 20:57:53 +0800

    Allowed copying from general to dates

--- V0.0/src/ido/main/CommandCopy.java
+++ V0.0/src/ido/main/CommandCopy.java
@@ -6,7 +6,12 @@ public class CommandCopy {
 	public static void copyTask(String source_date, String index,
 			String destination_date) {
 		
-		ArrayList<String> files = new FileAccessor(source_date+".txt").readContents();
+		ArrayList<String> files;
+		if(source_date.equals("-")){
+			files = new FileAccessor("general.txt").readContents();
+		}else{
+			files = new FileAccessor(source_date+".txt").readContents();
+		}
 		try{
 			String line = files.get(Integer.valueOf(index)-1);
 			String[] splitter = line.split("]");
