010d38bfe8c60ac644bfe18badc60ebc3481b482
Antony <karukapallyantony@msn.com>
2014-11-07 18:49:41 +0800

    Tidy up

--- V0.0/src/ido/main/CommandAdd.java
+++ V0.0/src/ido/main/CommandAdd.java
@@ -13,29 +13,30 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 //@author A0114813N
 
 public class CommandAdd {
-
-	private static Logger logger = Logger.getLogger("CommandAdd");
-
-	String date;
-	String task;
-	String time;
-	
 	private static final String ALL_DAY_TASK = "[by-today] ";
 	private static final int DATE_LENGTH = 6;
 	private static final int BOX_VALUE = 1;
 	private static final int TOTAL_BOX_VALUE = 7;
 	private static final String FLOATING_CHECK = "-";
-	
-	
+	private static final String ERROR_HEADING = "ERROR";
+	private static final String ERROR_TIME = "Invalid Time!";
+	private static final String MESSAGE_TASK = "Task Added!";
+	private static Logger logger = Logger.getLogger("CommandAdd");
+
+	String date;
+	String task;
+	String time;
+
 	public CommandAdd(String date, String time, String task) {
 		this.date = date;
 		try {
 			this.time = IsValidTime.validateTime(time);
 		} catch (Exception e) {
-			WarningPopUp.infoBox("Invalid Time!", "ERROR");
+			WarningPopUp.infoBox(ERROR_TIME, ERROR_HEADING);
 			e.printStackTrace();
 		}
 		this.task = task;
@@ -43,7 +44,7 @@ public class CommandAdd {
 
 	public void addTask() {
 		logger.log(Level.INFO, "add processing");
-		
+
 		// Create the name of the text file
 		String fileName = date + ".txt";
 		if (date.equals(FLOATING_CHECK)) {
@@ -53,7 +54,7 @@ public class CommandAdd {
 		if (time.equals(FLOATING_CHECK)) {
 			content = ALL_DAY_TASK + task;
 		}
-		
+
 		ArrayList<String> list = new ArrayList<String>();
 		// Check if the text file exists, if it does, add its content to
 		// a list and return the list
@@ -61,19 +62,20 @@ public class CommandAdd {
 		list = (new FileAccessor(fileName)).readContents();
 		list.add(content);
 		(new FileAccessor(fileName, list)).writeContents();
-		
+
 		boolean dateOutOfBox = true;
 		for (int i = 0; i < TOTAL_BOX_VALUE; i++) {
-			if (date.equals(DateModifier.getParticularDate(DateModifier.getCurrDate(), i))) {
+			if (date.equals(DateModifier.getParticularDate(
+					DateModifier.getCurrDate(), i))) {
 				dateOutOfBox = false;
 				break;
 			}
 		}
-		if(date.equals(FLOATING_CHECK)){
+		if (date.equals(FLOATING_CHECK)) {
 			dateOutOfBox = false;
 		}
 		if (dateOutOfBox) {
-			WarningPopUp.infoBox(content, "Task Added!");
+			WarningPopUp.infoBox(content, MESSAGE_TASK);
 		}
 		logger.log(Level.INFO, "add complete");
 	}
--- V0.0/src/ido/main/CommandCopy.java
+++ V0.0/src/ido/main/CommandCopy.java
@@ -3,21 +3,26 @@ package ido.main;
 import java.util.ArrayList;
 //@author A0114813N
 public class CommandCopy {
+	private static final String FLOATING_CHECK = "-";
+	private static final String GENERAL_FILE = "general.txt";
+	private static final String TXT_EXTENSION = ".txt";
+	private static final String ALL_DAY_TASK = "by-today";
+	
 	public static void copyTask(String source_date, String index,
 			String destination_date) {
 		
 		ArrayList<String> files;
-		if(source_date.equals("-")){
-			files = new FileAccessor("general.txt").readContents();
+		if(source_date.equals(FLOATING_CHECK)){
+			files = new FileAccessor(GENERAL_FILE).readContents();
 		}else{
-			files = new FileAccessor(source_date+".txt").readContents();
+			files = new FileAccessor(source_date + TXT_EXTENSION).readContents();
 		}
 		try{
 			String line = files.get(Integer.valueOf(index)-1);
 			String[] splitter = line.split("]");
 			String time = splitter[0].substring(1);
-			if(time.equals("all-day")){
-				time="-";
+			if(time.equals(ALL_DAY_TASK)){
+				time = FLOATING_CHECK;
 			}
 			String task = line.substring(splitter[0].length()+2); //']' and ' '
 			new CommandAdd(destination_date, time, task).addTask();
