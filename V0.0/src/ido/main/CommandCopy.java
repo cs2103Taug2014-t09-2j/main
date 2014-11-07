f58bd98a5b851775fe026a33790c163ee8eb5406
Antony <karukapallyantony@msn.com>
2014-11-07 18:50:23 +0800

    Cleaning up

--- V0.0/src/ido/main/CommandCopy.java
+++ V0.0/src/ido/main/CommandCopy.java
@@ -1,33 +1,35 @@
 package ido.main;
 
 import java.util.ArrayList;
+
 //@author A0114813N
 public class CommandCopy {
 	private static final String FLOATING_CHECK = "-";
 	private static final String GENERAL_FILE = "general.txt";
 	private static final String TXT_EXTENSION = ".txt";
 	private static final String ALL_DAY_TASK = "by-today";
-	
+
 	public static void copyTask(String source_date, String index,
 			String destination_date) {
-		
+
 		ArrayList<String> files;
-		if(source_date.equals(FLOATING_CHECK)){
+		if (source_date.equals(FLOATING_CHECK)) {
 			files = new FileAccessor(GENERAL_FILE).readContents();
-		}else{
-			files = new FileAccessor(source_date + TXT_EXTENSION).readContents();
+		} else {
+			files = new FileAccessor(source_date + TXT_EXTENSION)
+					.readContents();
 		}
-		try{
-			String line = files.get(Integer.valueOf(index)-1);
+		try {
+			String line = files.get(Integer.valueOf(index) - 1);
 			String[] splitter = line.split("]");
 			String time = splitter[0].substring(1);
-			if(time.equals(ALL_DAY_TASK)){
+			if (time.equals(ALL_DAY_TASK)) {
 				time = FLOATING_CHECK;
 			}
-			String task = line.substring(splitter[0].length()+2); //']' and ' '
+			String task = line.substring(splitter[0].length() + 2); // ']' and
+																	// ' '
 			new CommandAdd(destination_date, time, task).addTask();
-		}
-		catch (Exception e){
+		} catch (Exception e) {
 		}
 	}
 }
