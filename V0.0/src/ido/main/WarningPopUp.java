befdf7a6ba439967f981d4f7c3ae9885c28ed4a8
Made <radityapujamurti@gmail.com>
2014-11-08 23:07:24 +0800

    add comments in WarningPopUp class

--- V0.0/src/ido/main/WarningPopUp.java
+++ V0.0/src/ido/main/WarningPopUp.java
@@ -1,6 +1,13 @@
+//@author:A0113768Y
+
 package ido.main;
 import javax.swing.JOptionPane;
 
+/*
+ * A multipurpose class to display various message via pop up window
+ * Pre-cond: -
+ * Post-condition: displays a pop up window to display message
+ */	
 public class WarningPopUp {
 	
 	public static void infoBox(String infoMessage, String location){
