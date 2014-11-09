78e1abec4e6e7bcba0567a7cbc78c14157d066bf
chongwyb <chongwyb@gmail.com>
2014-11-09 23:51:43 +0800

    Improve code display

--- V0.0/src/ido/main/CommandDelete.java
+++ V0.0/src/ido/main/CommandDelete.java
@@ -17,50 +17,53 @@ public class CommandDelete {
 	int position = -1;
 	File file_object = null;
 	private static final String GENERALTXT = "general.txt";
+	private ArrayList<String> currDateTask = new ArrayList<>();
 
 	// Constructor
 	public CommandDelete(String date, String position) {
-		
+
 		this.date = IsValidDate.validateDate(date);
 		this.position = Integer.parseInt(position);
-		
+
 	}
 
 	// Mutator
 	public void delete() {
-		
+
 		if (position != -1) {
-			
+
 			// Specific task
 			if (date.equals("-")) {
 				fileName = GENERALTXT;
+				file_object = new File(fileName);
 				deleteSpecificTask();
-							
+
 			} else {
 				fileName = date + ".txt";
+				file_object = new File(fileName);
 				deleteSpecificTask();
-				
+
 			}
 
 		} else { // All Task
-			
+
 			if (date.equals("-")) {
 				fileName = GENERALTXT;
+				file_object = new File(fileName);
 				deleteAllTask();
-				
+
 			} else {
 				fileName = date + ".txt";
+				file_object = new File(fileName);
 				deleteAllTask();
-				
+
 			}
-		
+
 		}
 
 	}
 
 	private void deleteAllTask() {
-		ArrayList<String> currDateTask = new ArrayList<>();
-		file_object = new File(fileName);
 
 		// Test file
 		if (file_object.exists()) {
@@ -68,12 +71,12 @@ public class CommandDelete {
 			// read the content of the file, put in the list
 			currDateTask = (new FileAccessor(fileName)).readContents();
 
-			if (currDateTask.size() == 0) {				
+			if (currDateTask.size() == 0) {
 				WarningPopUp.infoBox("Nothing to clear", "WARNING");
 
 			} else {
 				currDateTask.clear();
-				
+
 			}
 
 			// write in file
@@ -87,22 +90,29 @@ public class CommandDelete {
 	}
 
 	private void deleteSpecificTask() {
-		ArrayList<String> currDateTask = new ArrayList<>();
 
-		// read the content of the file, put in the list
-		currDateTask = (new FileAccessor(fileName)).readContents();
+		// Test file
+		if (file_object.exists()) {
+			
+			// read the content of the file, put in the list
+			currDateTask = (new FileAccessor(fileName)).readContents();
 
-		// check if valid
-		if (position - 1 < currDateTask.size()) {
-			currDateTask.remove(position - 1);			
+			// check if valid
+			if (position - 1 < currDateTask.size()) {
+				currDateTask.remove(position - 1);
+
+			} else {
+				WarningPopUp.infoBox("Unable to delete", "WARNING");
+
+			}
+
+			// write in file
+			(new FileAccessor(fileName, currDateTask)).writeContents();
 
 		} else {
 			WarningPopUp.infoBox("Unable to delete", "WARNING");
 
 		}
-
-		// write in file
-		(new FileAccessor(fileName, currDateTask)).writeContents();
 
 	}
 }
